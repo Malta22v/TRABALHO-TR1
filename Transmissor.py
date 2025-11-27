53eadea1942cb99622c2488e77494d5932dd50a4
ogmoises <moisesaltouniann@gmail.com>
2025-11-27 04:27:14 -0300

    melhorias

--- Transmissor.py
+++ Transmissor.py
@@ -1,23 +1,27 @@
-import socket
 import time
 from bitarray import bitarray
+import socket
 
 def startServer(message, host='127.0.0.1', port=12345, max_retries=3):
+    """
+    Envia uma sequência de bits para um servidor TCP.
+    Realiza múltiplas tentativas caso o servidor não esteja disponível.
+    """
 
-    #Transforma a mensagem recebida em uma sequência de bytes para ser enviada
-    bit_data = bitarray()
+    bit_data = bitarray() #Transforma array de bits em dados
     bit_data.extend(message)
     byte_array = bit_data.tobytes()
     
-    for attempt in range(max_retries):
-        try:    #realiza diferentes tentativas de conexão e envio
+    # Loop de tentativas de conexão
+    for tentativa in range(maximo_de_tentativas):
+        try: # Cria o socket dentro do bloco 'with' para fechar automaticamente
             with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                 s.connect((host, port))
                 s.sendall(byte_array)
-                print("Dados enviados com sucesso!")
+                print("Envio de dados concluído!")
                 return True
         except ConnectionRefusedError:
-            print(f"Tentativa {attempt + 1}/{max_retries} - Servidor não disponível. Tentando novamente...")
+            print(f"Tentativa {tentativa + 1}/{maximo_de_tentativas} - Servidor com ero...")
             time.sleep(1)
-    print("Falha ao conectar ao servidor após várias tentativas.")
+    print("Não foi possível estabelecer conexão após todas as tentativas.")
     return False
